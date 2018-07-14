6b4c16aabc886fa0d7ebf362977e9bf4546341b5
jacprada <jacopo.prada@gmail.com>
2018-07-14 18:44:51 +0100

    core components generated

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,19 @@ import { NgModule } from '@angular/core';
 
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { FilterComponent } from './filter/filter.component';
+import { CollectionComponent } from './collection/collection.component';
+import { FooterComponent } from './footer/footer.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FilterComponent,
+    CollectionComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule
